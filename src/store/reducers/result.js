01ead2cc464c745fb36e515887d4e05f8a435544
Linead96 <danielgarboza@yahoo.com>
2019-06-12 22:38:26 +0800

    Added updateObject utility function for redux

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,4 +1,5 @@
 import * as actionTypes from "../actions/actionTypes";
+import { updateObject } from "../utility.js";
 
 const initialState = {
   results: []
@@ -7,19 +8,13 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE_RESULT: {
-      return {
-        ...state,
-        results: [...state.results, { id: new Date(), value: action.result }]
-      };
+      return updateObject(state, {results: [...state.results, { id: new Date(), value: action.result }]} )
     }
     case actionTypes.DELETE_RESULT: {
       const updatedArray = state.results.filter(result => {
         return action.resultElId !== result.id;
       });
-      return {
-        ...state,
-        results: updatedArray
-      };
+      return updateObject(state, {results: updatedArray});
     }
     default: {
       return state;
